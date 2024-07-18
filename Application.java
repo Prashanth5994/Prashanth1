3ec86cb04370b26dd25f8e0f24f2a003eacb337d
Prashanth5994 <prasanth.ch1604@gmail.com>
2024-07-18 15:22:06 +0530

    feature202 changes in Application.java

--- Application.java
+++ Application.java
@@ -18,4 +18,22 @@ class Main {
     int result = obj.addNumbers(num1, num2);
     System.out.println("Sum is: " + result);
   }
+  #feature202 changes in payment gateway
+ public class Main {
+
+  // method defined
+  private static int getSquare(int x){
+    return x * x;
+  }
+
+  public static void main(String[] args) {
+    for (int i = 1; i <= 5; i++) {
+
+      // method call
+      int result = getSquare(i);
+      System.out.println("Square of " + i + " is: " + result);
+    }
+  }
+}
+#feature202 ends 
 }
